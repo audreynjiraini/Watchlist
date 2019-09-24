0af4177210629b44295ddbcff9e4c2c2e45cee67
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-24 15:46:55 +0300

    idk

--- app/models.py
+++ app/models.py
@@ -47,39 +47,7 @@ class Review(db.Model):
     def get_reviews(cls, id):
         reviews = Review.query.filter_by(movie_id = id).all()
         return reviews
-    
-# class Review:
 
-#     all_reviews = []
-
-#     def __init__(self,movie_id,title,imageurl,review):
-#         self.movie_id = movie_id
-#         self.title = title
-#         self.imageurl = imageurl
-#         self.review = review
-
-
-#     def save_review(self):
-#         Review.all_reviews.append(self)
-
-
-#     @classmethod
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
-
-#     @classmethod
-#     def get_reviews(cls,id):
-
-#         response = []
-
-#         for review in cls.all_reviews:
-#             if review.movie_id == id:
-#                 response.append(review)
-
-#         return response
- 
- 
- 
  
 class User(UserMixin,db.Model): # create User class and pass in db.Model as an argument to connect our class to our database and allow communication
     
